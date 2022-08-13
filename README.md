19519f79e019c453c73e6c29891e647b73691b48
KalmeMarq <marq520filip56.fm@gmail.com>
2022-08-13 19:18:56 +0100

    first commit for infdev

---
+++ README.md
@@ -0,0 +1,3 @@
+# KM LegacyGUI Mod (Fabric Minecraft Infdev 20100618)
+
+### Reimplementing features from the 0.30 branch
---
+++ src/main/java/me/kalmemarq/legacygui/LegacyGUIMod.java
@@ -0,0 +1,11 @@
+package me.kalmemarq.legacygui;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LegacyGUIMod implements ModInitializer {
+	public static final String MOD_ID = "kmlegacygui";
+
+	@Override
+	public void onInitialize() {
+	}
+}
